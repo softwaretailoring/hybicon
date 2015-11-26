a28eca4ad12dba6616d630cd71b7b78b4a19b994
Gábor Berkesi <gabor.berkesi@softwaretailoring.net>
2015-11-26 03:56:29 +0100

    Refactor hover, click and info mode New hover and click modes: switch, rotate

--- js/for-doc/index.js
+++ js/for-doc/index.js
@@ -15,19 +15,16 @@ var setHybicons = function () {
     }
 
     var hovermode = getQueryVariable("hovermode");
-    if (hovermode !== "") { setMode("hovermode"); }
+    if (hovermode !== "") { setMode("hovermode", hovermode); }
 
     var clickmode = getQueryVariable("clickmode");
-    if (clickmode !== "") { setMode("clickmode"); }
+    if (clickmode !== "") { setMode("clickmode", clickmode); }
 
     var infomode = getQueryVariable("infomode");
-    if (infomode !== "") { setMode("infomode"); }
-
-    var inforight = getQueryVariable("inforight");
-    if (inforight !== "") { setMode("inforight"); }
+    if (infomode !== "") { setMode("infomode", infomode); }
 };
 
-var setMode = function (mode) {
+var setMode = function (mode, modevalue) {
     var hybicons = document.querySelectorAll('[data-hybicon]');
     
     for (var i = 0; i < hybicons.length; i++) {
@@ -35,6 +32,7 @@ var setMode = function (mode) {
         if (hybiconId !== 'userideaheader' &&
             hybiconId !== 'checkboxHover' &&
             hybiconId !== 'checkboxClick' &&
+            hybiconId !== 'checkboxInfo' &&
             hybiconId !== 'iconGitHub' &&
             hybiconId !== 'iconTwitter' &&
             hybiconId !== 'iconMail') {
@@ -43,7 +41,10 @@ var setMode = function (mode) {
                 icon.removeAttribute('data-hybicon-' + mode );
             }
             else {
-                icon.setAttribute('data-hybicon-' + mode, '');
+                icon.setAttribute('data-hybicon-' + mode, modevalue);
+                if (mode === "infomode") {
+                    icon.setAttribute('data-hybicon-infotext', 'info');
+                }
             }
             new hybicon(hybiconId);
         }
@@ -91,3 +92,57 @@ var setStyle = function () {
     }
 };
 
+var setGitHubSize = function () {
+    var divGitHubStar = document.getElementById("divGitHubStar");
+    var divGitHubFork = document.getElementById("divGitHubFork");
+    var divGitHubWatch = document.getElementById("divGitHubWatch");
+    var divGitHubIssue = document.getElementById("divGitHubIssue");
+    var divGitHubDownload = document.getElementById("divGitHubDownload");
+
+    var infomode = "";
+    var hovermode = "";
+    var currentClass = "";
+    var nextClass = "";
+
+    if (divGitHubStar.getAttribute("data-hybicon-infomode") === "") {
+        infomode = "right";
+        hovermode = "switch";
+        currentClass = "hybicon150";
+        nextClass = "hybicon30";
+    }
+    else {
+        infomode = "";
+        hovermode = "rotate";
+        currentClass = "hybicon30";
+        nextClass = "hybicon150";
+    }
+
+    // set infomode
+    divGitHubStar.setAttribute("data-hybicon-infomode", infomode);
+    divGitHubFork.setAttribute("data-hybicon-infomode", infomode);
+    divGitHubWatch.setAttribute("data-hybicon-infomode", infomode);
+    divGitHubIssue.setAttribute("data-hybicon-infomode", infomode);
+    divGitHubDownload.setAttribute("data-hybicon-infomode", infomode);
+    
+    // set hovermode
+    divGitHubStar.setAttribute("data-hybicon-hovermode", hovermode);
+    divGitHubFork.setAttribute("data-hybicon-hovermode", hovermode);
+    divGitHubWatch.setAttribute("data-hybicon-hovermode", hovermode);
+    divGitHubIssue.setAttribute("data-hybicon-hovermode", hovermode);
+    divGitHubDownload.setAttribute("data-hybicon-hovermode", hovermode);
+
+    // set size
+    divGitHubStar.classList.remove(currentClass);
+    divGitHubFork.classList.remove(currentClass);
+    divGitHubWatch.classList.remove(currentClass);
+    divGitHubIssue.classList.remove(currentClass);
+    divGitHubDownload.classList.remove(currentClass);
+
+    divGitHubStar.classList.add(nextClass);
+    divGitHubFork.classList.add(nextClass);
+    divGitHubWatch.classList.add(nextClass);
+    divGitHubIssue.classList.add(nextClass);
+    divGitHubDownload.classList.add(nextClass);
+
+    hybiconsgithub();
+};
