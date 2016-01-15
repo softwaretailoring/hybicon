072844ca1a175442c9494f4af46644f0d22c4125
Gábor Berkesi <gabor.berkesi@softwaretailoring.net>
2016-01-15 23:51:42 +0100

    Modify files to 2016

--- js/hybicon.icons.js
+++ js/hybicon.icons.js
@@ -7,7 +7,7 @@
 /* Check http://hybicon.softwaretailoring.net/icons.html for samples.                      */
 /* Fork https://github.com/softwaretailoring/hybicon for contribution.                     */
 /* ======================================================================================= */
-/* Copyright © 2015 Gábor Berkesi (http://softwaretailoring.net)                           */
+/* Copyright © 2015-2016 Gábor Berkesi (http://softwaretailoring.net)                      */
 /* Licensed under MIT (https://github.com/softwaretailoring/hybicon/blob/master/LICENSE)   */
 /* ======================================================================================= */
 
